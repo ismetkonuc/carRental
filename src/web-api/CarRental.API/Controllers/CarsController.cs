f7e3bd24bd76d0074f7caf3721e8c2bbd45ef008
İsmet KONUÇ <ismetkonuc@gmail.com>
2022-03-15 01:29:50 +0300

    create API layer

---
+++ src/web-api/CarRental.API/Controllers/CarsController.cs
@@ -0,0 +1,29 @@
+﻿using CarRental.Business.Interfaces;
+using CarRental.Entities.Concrete;
+using CarRental.Entities.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+
+        private readonly ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+
+        [HttpGet]
+        public string Get()
+        {
+            return "CarsController";
+
+        }
+    }
+}
