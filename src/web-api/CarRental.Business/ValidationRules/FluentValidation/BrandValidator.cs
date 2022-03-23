b9e29385f69b1d7d8b3b3987fb02f5776aecdac9
İsmet KONUÇ <ismetkonuc@gmail.com>
2022-03-24 00:44:17 +0300

    add brand and rental validators

---
+++ src/web-api/CarRental.Business/ValidationRules/FluentValidation/BrandValidator.cs
@@ -0,0 +1,13 @@
+﻿using CarRental.Entities.Concrete;
+using FluentValidation;
+
+namespace CarRental.Business.ValidationRules.FluentValidation
+{
+    public class BrandValidator : AbstractValidator<Brand>
+    {
+        public BrandValidator()
+        {
+            RuleFor(I => I.Name).NotEmpty();
+        }
+    }
+}
---
+++ src/web-api/CarRental.Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -0,0 +1,14 @@
+﻿using CarRental.Entities.Concrete;
+using FluentValidation;
+
+namespace CarRental.Business.ValidationRules.FluentValidation
+{
+    public class RentalValidator : AbstractValidator<Rental>
+    {
+        public RentalValidator()
+        {
+            RuleFor(I => I.ReturnDate).GreaterThanOrEqualTo(I => I.RentDate);
+            RuleFor(I => I.RentDate).LessThanOrEqualTo(I => I.ReturnDate);
+        }
+    }
+}
