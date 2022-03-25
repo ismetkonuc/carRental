ba0034378fa5bc21aff461f8f83e17bd80428ba9
İsmet KONUÇ <ismetkonuc@gmail.com>
2022-03-26 01:24:10 +0300

    add caching

---
+++ src/web-api/CarRental.Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -0,0 +1,25 @@
+﻿using CarRental.Core.CrossCuttingConcerns.Caching;
+using CarRental.Core.Utils.Interceptors;
+using CarRental.Core.Utils.IoC;
+using Castle.DynamicProxy;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarRental.Core.Aspects.Autofac.Caching
+{
+    public class CacheRemoveAspect : MethodInterception
+    {
+        private string _pattern;
+        private ICacheManager _cacheManager;
+
+        public CacheRemoveAspect(string pattern)
+        {
+            _pattern = pattern;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            _cacheManager.RemoveByPattern(_pattern);
+        }
+    }
+}
---
+++ src/web-api/CarRental.Core/CrossCuttingConcerns/Caching/ICacheManager.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRental.Core.CrossCuttingConcerns.Caching
+{
+    public interface ICacheManager
+    {
+        T Get<T> (string key);
+        void Add(string key, object value, int duration);
+        void Remove(string key);
+        bool IsAdd(string key);
+        void RemoveByPattern(string pattern);
+    }
+}
